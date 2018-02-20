7080d33a0cd43ebc11e08601ca85dd0778881aaa
Jiangyi Lin <lin.jian@husky.neu.edu>
2018-02-19 23:41:28 -0500

    Log in and register

--- lib/task_tracker_web/router.ex
+++ lib/task_tracker_web/router.ex
@@ -7,6 +7,14 @@ defmodule TaskTrackerWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :get_current_user
+  end
+
+  def get_current_user(conn, _params) do
+    # TODO: Move this function out of the router module.
+    user_id = get_session(conn, :user_id)
+    user = TaskTracker.Accounts.get_user(user_id || -1)
+    assign(conn, :current_user, user)
   end
 
   pipeline :api do
@@ -17,8 +25,12 @@ defmodule TaskTrackerWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
     resources "/users", UserController
     resources "/tasks", TaskController
+
+    post "/session", SessionController, :create
+    delete "/session", SessionController, :delete
   end
 
   # Other scopes may use custom stacks.
